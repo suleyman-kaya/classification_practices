102b47703fefa0b388c3c72370bd21ecc67c7a06
suleyman <suleymankayaxyz@gmail.com>
2023-04-02 14:09:14 +0300

    upload files

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# classification_practices" >> README.md
+git init
+git add *
+git commit -m "upload files"
+git branch -M main
+git remote add origin https://github.com/suleyman-kaya/classification_practices.git
+git push -u origin main
